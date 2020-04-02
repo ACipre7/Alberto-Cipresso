8635599ddb2f01cef28ba7bef979071bd4aea397
ACipre7 <61702056+ACipre7@users.noreply.github.com>
2020-04-02 12:38:51 +0200

    Start commit

---
+++ sumDigits.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Alberto Cipresso on 01/04/20.
+//
+
+#include <string>
+#include "sumDigits.h"
+
+using namespace std;
+
+
+int sumOfDigits(int n) {
+    string s = to_string(n);
+
+    int sum = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        sum += s[i] - 48;
+    }
+
+    return sum;
+}
+
+int sumOfDigits(int n, int arr[], int nDigits) {
+    int dix1 = 1;
+    int dix2 = 10;
+    for (int i = 0; i < nDigits; ++i) {
+        arr[i] = (n % dix2) / dix1;
+        // 1234 -> % 10 = 4 --> / 1 --> 4
+        dix1 *= 10;
+        dix2 *= 10;
+    }
+
+    int sum = 0;
+    for (int j = 0; j < nDigits; ++j) {
+        sum += arr[j];
+    }
+
+    return sum;
+}
